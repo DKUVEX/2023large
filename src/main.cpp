618bd337ce43f7604fa0d032a4dc558d982fc552
unknown <songsongeugene@gmail.com>
2023-02-18 23:49:42 +0800

    Realize turning and moving.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "main.h"
-
+using namespace pros;
 /**
  * A callback function for LLEMU's center button.
  *
@@ -75,18 +75,22 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	Motor left_mtr_A(20);
+    Motor right_mtr_A(11);
+    Motor left_mtr_B(9);
+    Motor right_mtr_B(8);
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+		int forward = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+		int turn = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
 
-		left_mtr = left;
-		right_mtr = right;
+		left_mtr_A = forward+turn;
+        left_mtr_B= forward+turn;
+        right_mtr_A = -forward+turn;
+		right_mtr_B = -forward+turn;
 
 		pros::delay(20);
 	}
